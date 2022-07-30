b47876c30371870e7ea45801263a2232b4543f5d
Dict Xiong <me@beardic.cn>
2022-07-31 02:11:19 +0800

    restruct; get-my-ip

--- frigg/__init__.py
+++ frigg/__init__.py
@@ -1,4 +1,27 @@
-from flask import Flask
+from flask import Flask, abort, request
+from frigg import var
 app = Flask(__name__)
 
-import frigg.route
+
+@app.route('/')
+def hello_world():
+    return '<h1>Welcome to api.beardic.cn</h1>\n'
+
+
+@app.route('/get-var/<path:var_path>')
+def get_var(var_path):
+    ret = var.get_var(var_path)
+    if ret is not None:
+        return ret
+    else:
+        abort(404)
+
+
+@app.route('/get-my-ip')
+def get_my_ip():
+    return request.remote_addr
+
+
+@app.errorhandler(404)
+def handle_404(error):
+    return "404 not found", 404
--- frigg/var.py
+++ frigg/var.py
@@ -5,7 +5,8 @@ var_dir = os.path.join(os.path.dirname(__file__), 'var')
 if not os.path.exists(var_dir):
     var_dir = os.path.join(os.path.dirname(__file__), 'var-example')
 
-def get_var(var_path:str):
+
+def get_var(var_path: str):
     var_full_path = os.path.realpath(os.path.join(var_dir, var_path))
     if not var_full_path.startswith(var_dir) or not os.path.exists(var_full_path):
         return None
