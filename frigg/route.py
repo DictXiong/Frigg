ce908eddea92cedb90e2b58052ca666cad2aaac0
Dict Xiong <me@beardic.cn>
2022-07-30 11:52:20 +0800

    unify to get-var

--- frigg/route.py
+++ frigg/route.py
@@ -1,13 +1,18 @@
 from frigg import app
 from frigg import var
+import logging
 
 @app.route('/')
 def hello_world():
     return '<h1>Welcome to api.beardic.cn</h1>\n'
 
 
-@app.route('/get-dfs-commit/')
-def get_dfs_commit():
-    dfs_commit = var.get_var('dfs-commit')
-    return dfs_commit
+@app.route('/get-var/<path:var_path>')
+def get_var(var_path):
+    logging.info(f"Request var: {var_path}")
+    ret = var.get_var(var_path)
+    if ret is not None:
+        return ret
+    else:
+        return "404 not found"
     
--- frigg/var.py
+++ frigg/var.py
@@ -4,10 +4,11 @@ import os, logging
 var_dir = os.path.join(os.path.dirname(__file__), 'var')
 if not os.path.exists(var_dir):
     var_dir = os.path.join(os.path.dirname(__file__), 'var-example')
-def get_var(var_name:str):
-    var_path = os.path.join(var_dir, var_name)
-    if not os.path.exists(var_path):
-        logging.error('Var file %s does not exist.' % var_name)
+
+def get_var(var_path:str):
+    var_full_path = os.path.join(var_dir, var_path)
+    if not os.path.exists(var_full_path):
+        logging.warning('Request var %s does not exist.' % var_path)
         return None
-    with open(var_path, 'r') as f:
+    with open(var_full_path, 'r') as f:
         return str(f.read()).strip()
