015f42479a81966b55616f218b7d88f3e416abc8
Dict Xiong <me@beardic.cn>
2024-12-13 11:39:10 +0800

    build: update to cloudflare api v4

--- frigg/ddns.py
+++ frigg/ddns.py
@@ -1,24 +1,25 @@
 # pylint: disable=W,C,R
 import time
-import CloudFlare
-import CloudFlare.exceptions
+import cloudflare
 
 
 class CFClient:
     def __init__(self, config, logger, pusher):
-        self.cf = CloudFlare.CloudFlare(token=config['token'])
+        self.cf = cloudflare.Cloudflare(api_token=config['token'])
         self.logger = logger
         self.pusher = pusher
         try:
-            params = {'name': config['zone']}
-            zones = self.cf.zones.get(params=params)
-        except CloudFlare.exceptions.CloudFlareError as e:
+            zones = self.cf.zones.list(name=config['zone'])
+        except cloudflare.CloudflareError as e:
             self.logger.fatal('error: %s - api call failed' % (e))
-        if len(zones) != 1:
-            self.logger.fatal('error: %s - api call returned %d zones' % (config['zone'], len(zones)))
-        self.zone = zones[0]
-        self.zone_name = self.zone['name']
-        self.zone_id = self.zone['id']
+        zone = None
+        for i in zones:
+            if zone is not None:
+                self.logger.fatal('error: %s - api call returned >1 zones' % (config['zone']))
+            zone = i
+        self.zone = zone
+        self.zone_name = self.zone.name
+        self.zone_id = self.zone.id
         self.logger.info('zone %s found' % (self.zone_name))
 
 
@@ -28,20 +29,19 @@ class CFClient:
             updated = False
             if not ip:
                 updated = True
-            params = {'name': name, 'match': 'all', 'type': ip_type}
-            dns_records = self.cf.zones.dns_records.get(self.zone_id, params=params)
+            dns_records = self.cf.dns.records.list(zone_id=self.zone_id, name=name, type=ip_type, match="all")
             for dns_record in dns_records:
-                assert dns_record['type'] == ip_type and dns_record['name'] == name
+                assert dns_record.type == ip_type and dns_record.name == name
                 if updated:
-                    self.cf.zones.dns_records.delete(self.zone_id, dns_record['id'])
+                    self.cf.dns.records.delete(zone_id=self.zone_id, dns_record_id=dns_record.id)
                     self.logger.info('record %s deleted' % (name))
                     continue
-                if dns_record['content'] == ip:
+                if dns_record.content == ip:
                     self.logger.info('record %s not changed with %s' % (name, ip))
                     updated = True
                     continue
-                dns_record_id = dns_record['id']
-                original_ip = dns_record['content']
+                dns_record_id = dns_record.id
+                original_ip = dns_record.content
                 dns_record = {
                     'name': name,
                     'type': ip_type,
@@ -49,7 +49,7 @@ class CFClient:
                     'proxied': False,
                     'comment': f'FRIGG {time.strftime("%Y-%m-%d %H:%M:%S")}'
                 }
-                dns_record = self.cf.zones.dns_records.put(self.zone_id, dns_record_id, data=dns_record)
+                dns_record = self.cf.dns.records.update(zone_id=self.zone_id, dns_record_id=dns_record_id, **dns_record)
                 self.logger.info('record %s updated to %s' % (name, ip))
                 if self.pusher:
                     self.pusher.push_dns_updated(name, ip, original_ip)
@@ -62,7 +62,7 @@ class CFClient:
                     'proxied': False,
                     'comment': f'FRIGG {time.strftime("%Y-%m-%d %H:%M:%S")}'
                 }
-                self.cf.zones.dns_records.post(self.zone_id, data=dns_record)
+                self.cf.dns.records.create(self.zone_id, **dns_record)
                 self.logger.info('record %s created with %s' % (name, ip))
                 if self.pusher:
                     self.pusher.push_dns_updated(name, ip)
@@ -74,7 +74,7 @@ class CFClient:
             self._aud_record(f"{name}.0.{self.zone_name}", ip4, ip6)
             self._aud_record(f"{name}.4.{self.zone_name}", ip4=ip4)
             self._aud_record(f"{name}.6.{self.zone_name}", ip6=ip6)
-        except CloudFlare.exceptions.CloudFlareError as e:
+        except cloudflare.CloudFlareError as e:
             self.logger.error('error: %s - api call failed' % (e))
             return False
         return True
